87ab4d17ef95f2078d29a7c4266de362c8bc9601
Anthony Victor <thonnyvrc@gmail.com>
2023-02-08 12:54:21 -0300

    feat: added database from frontend

--- src/config/api.ts
+++ src/config/api.ts
@@ -2,9 +2,9 @@ import axios from "axios";
 import { environments } from "./dotenv";
 
 export const api = axios.create({
-	baseURL: environments.apiURL,
-	headers: {
-		"Content-Type": "application/json",
-		Authorization: Buffer.from(environments.apiKey).toString("base64"),
-	},
+  baseURL: environments.apiURL,
+  headers: {
+    "Content-Type": "application/json",
+    Authorization: Buffer.from(environments.apiKey).toString("base64"),
+  },
 });
--- src/config/dotenv.ts
+++ src/config/dotenv.ts
@@ -2,11 +2,11 @@ import { config } from "dotenv";
 import path from "path";
 
 config({
-	path: path.join(__dirname, "..", "..", ".env"),
+  path: path.join(__dirname, "..", "..", ".env"),
 });
 
 export const environments = {
-	Port: process.env.APP_PORT as unknown as number,
-	apiURL: process.env.API_URL as string,
-	apiKey: process.env.API_KEY as string,
+  Port: process.env.APP_PORT as unknown as number,
+  apiURL: process.env.API_URL as string,
+  apiKey: process.env.API_KEY as string,
 };
