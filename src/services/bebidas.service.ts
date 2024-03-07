3e3fe3f3adabb1f9883666948171932adf24a3e7
thonnykjjjkk <thonnyvrc@gmail.com>
2024-03-07 02:00:13 -0300

    fix: sort bebidas

--- src/services/bebidas.service.ts
+++ src/services/bebidas.service.ts
@@ -33,8 +33,8 @@ export class BebidasService extends Service<IOutro> {
   async find(): Promise<IOutro[]> {
     const data = ((await this.repo.find()) as IOutro[])
       .sort((a, b) => (a.nome > b.nome ? 1 : -1))
-      .sort(this.sortBySize)
-      .sort(this.sortByType);
+      .sort(this.sortByType)
+      .sort(this.sortBySize);
     return data;
   }
 
